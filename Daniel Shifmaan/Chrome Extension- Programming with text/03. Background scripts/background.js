27a9c1e5edfac523d234d8ff7f1ce6607fce908a
mohammedyaseen2211 <ysn6040@gmail.com>
2023-08-12 22:12:37 +0530

    added

---
+++ Daniel Shifmaan/Chrome Extension- Programming with text/03. Background scripts/background.js
@@ -0,0 +1,1 @@
+console.log("background color is running")
---
+++ Daniel Shifmaan/Chrome Extension- Programming with text/03. Background scripts/content.js
@@ -0,0 +1,18 @@
+console.log("chrome extension go?");
+
+
+let paragraphs = document.getElementsByTagName("p");
+
+for(elt of paragraphs){
+    elt.style['background-color'] = "#ff0011";
+}
+
+
+/*
+- You can use these urls in matches
+1. '*://*shiffman*'
+2. '*://*github*'
+3. 'http://shiffaman.net/*
+4. 'https://shiffaman.net/*'
+Note; it will match for only that links.
+*/
